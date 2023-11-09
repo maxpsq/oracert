57b90ddfd72b39d811333bbc06824bb3b50ba5ee
Massimo Pasquini <m.pasquini@liscor.com>
2023-11-09 15:31:35 +0100

    update

--- 1Z0-144/Oracle_PLSQL/02.Declaring_PLSQL_Variables/02.01.Recognize_valid_and_invalid_identifiers.sql
+++ 1Z0-144/Oracle_PLSQL/02.Declaring_PLSQL_Variables/02.01.Recognize_valid_and_invalid_identifiers.sql
@@ -3,16 +3,40 @@
 =================================================================================
 OBJECTIVE: Recognize valid and invalid identifiers
 =================================================================================
-Valid variable names are bound to the same rules as database objects
+Valid ordinary user-defined identifiers are bound to the same rules as database objects
 They must 
   - be composed of characters from the database charactr set
   - begin with a letter 
-  - contain a letters, digits or the symbols $ _ #
-  - not exceed 30 characters (30 bytes) in length
+  - contain letters, digits or the symbols $ _ #
+  - not exceed 30 bytes in length
 
-A valid PL/SQL variable can be validated against the following regular expression
+A valid ordinary user-defined identifier can be validated against the following
+regular expression
 
 ^[A-Za-z][A-Za-z0-9$#_]{,29}$
 
 */
 
+declare
+  "new variable"   number;
+  " trailing spaces "   number;
+  "A+B=C"   number;
+  "??????????"   number;
+begin
+  "new variable" := 2;
+end;
+/
+
+/*
+For quoted identifiers, all characters are allowed except
+- double quotes
+- new lines
+- null character
+*/
+declare
+  "4
+  long"   number;
+begin
+  null;
+end;
+/
